a1ec8a60515958e0f7e660c5170f4e3ddfb72d47
Alex <alex@maximum.guru>
2023-09-11 20:30:38 -0700

    Improved email variables processing code

--- src/integration/wc-variables-processor.php
+++ src/integration/wc-variables-processor.php
@@ -142,13 +142,95 @@
 			 */
 			private function process_email_variables( $content )
 			{
-				if( ! $this->email )
-					return $content;
+				if( is_a( $this->email, 'WC_Email' ) )
+					// this will process {blogname} (hardcoded in format_string),
+					// {site_title}, {site_address}, {site_url} (hardcoded in class WC_Email)
+					// and tags from woocommerce_email_format_string, woocommerce_email_format_string_find and woocommerce_email_format_string_replace filters
+					return $this->email->format_string( $content );
 				
-				// this will process {blogname} (hardcoded in format_string),
-				// {site_title}, {site_address}, {site_url} (hardcoded in class WC_Email)
-				// and tags from woocommerce_email_format_string_find and woocommerce_email_format_string_replace filters
-				return $this->email->format_string( $content );
+				if( is_a( $this->order, 'WC_Order' ) )
+				{
+					// determine $email_class
+					$status = $this->order->get_status();
+					// TODO: switch to a better way to determine the email class
+					$email_class = 'WC_Email_New_Order';
+					switch($status)
+					{
+						case 'pending':
+							$email_class = 'WC_Email_Customer_Pending_Order';
+							break;
+						case 'processing':
+							$email_class = 'WC_Email_Customer_Processing_Order';
+							break;
+						case 'on-hold':
+							$email_class = 'WC_Email_Customer_On_Hold_Order';
+							break;
+						case 'completed':
+							$email_class = 'WC_Email_Customer_Completed_Order';
+							break;
+						case 'cancelled':
+							$email_class = 'WC_Email_Cancelled_Order';
+							break;
+						case 'refunded':
+							$email_class = 'WC_Email_Customer_Refunded_Order';
+							break;
+						case 'failed':
+							$email_class = 'WC_Email_Customer_Failed_Order';
+							break;
+						case 'checkout-draft':
+							$email_class = 'WC_Email_New_Order';
+							break;
+					}
+					
+					// create email template corresponds to order status based on $email_id
+					$emails = &WC()->mailer()->emails;
+					if( isset( $emails[$email_class] ) && is_a( ( $email = $emails[$email_class] ), 'WC_Email' ) )
+					{
+						$email->object = $this->order;
+						$email->recipient = $this->order->get_billing_email();
+						
+						// we can't trigger() because that will send the email message, so we have to manually set the placeholders that are made available with trigger()
+						$placeholders = array(
+							'{order_date}' => wc_format_datetime( $this->order->get_date_created() ),
+							'{order_number}' => $this->order->get_order_number(),
+							'{order_billing_full_name}' => $this->order->get_formatted_billing_full_name(),
+						);
+						
+						$find = array_keys( $placeholders );
+						$replace = array_values( $placeholders );
+						
+						$email->find = array_merge( (array) $email->find, $find );
+						$email->replace = array_merge( (array) $email->replace, $replace );
+						
+						return $email->format_string( $content );
+					}
+				}
+				
+				// if the email and order instances are not available, try to replace some placeholders that are possible to replace
+				$blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
+				$blogurl = wp_parse_url( home_url(), PHP_URL_HOST );
+				$placeholders = array(
+					'{blogname}' => $blogname,
+					'{site_title}' => $blogname,
+					'{site_address}' => $blogurl,
+					'{site_url}' => $blogurl,
+				);
+				
+				$find    = array_keys( $placeholders );
+				$replace = array_values( $placeholders );
+				
+				// TODO: figure out how to run the following filters without having the $email instance
+				
+				// trigger woocommerce_email_format_string_find and woocommerce_email_format_string_replace filters
+				// $find = apply_filters( 'woocommerce_email_format_string_find', $find, $email );
+				// $replace = apply_filters( 'woocommerce_email_format_string_replace', $replace, $email );
+				
+				$content = str_replace( $find, $replace, $content );
+				
+				// trigger woocommerce_email_format_string filter
+				// $content = apply_filters( 'woocommerce_email_format_string', $content, $email );
+				
+				return $content;
 			}
 			
 			/**
