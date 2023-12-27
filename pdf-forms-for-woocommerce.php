fe1ab894df80a836b7cb24d25ae678d40c6d1013
Alex <alex@maximum.guru>
2023-12-27 15:47:56 -0800

    Added error check

--- pdf-forms-for-woocommerce.php
+++ pdf-forms-for-woocommerce.php
@@ -1433,7 +1433,8 @@ if( ! class_exists( 'Pdf_Forms_For_WooCommerce', false ) )
 							if( $parsed !== false )
 							{
 								$url_mimetype = $parsed['mime'];
-								file_put_contents( $filepath, $parsed['data'] );
+								if( file_put_contents( $filepath, $parsed['data'] ) === false || ! is_file( $filepath ) )
+									throw new Exception( __( "Failed to create file", 'pdf-forms-for-woocommerce' ) );
 							}
 						}
 					}
